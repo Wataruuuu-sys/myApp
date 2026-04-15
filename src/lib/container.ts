f0f679f88a1919b77206d0652e5006bfd90f63ed
wataru <ws1021wataru11@gmail.com>
2026-04-15 10:38:09 +0900

    todo deleted

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -1,6 +1,3 @@
-import { TodoRepository } from "@/repositories/TodoRepository"
-import { TodoUsecase } from "@/usecases/TodoUsecase"
-import type { ITodoUsecase } from "@/usecases/IUsecase/ITodoUsecase"
 import { TopicRepository } from "@/repositories/TopicRepository"
 import { TopicUsecase } from "@/usecases/TopicUsecase"
 import type { ITopicUsecase } from "@/usecases/IUsecase/ITopicUsecase"
@@ -10,9 +7,6 @@ import type { IAnswerUsecase } from "@/usecases/IUsecase/IAnswerUsecase"
 import { PredictionRepository } from "@/repositories/PredictionRepository"
 import { PredictionUsecase } from "@/usecases/PredictionUsecase"
 import type { IPredictionUsecase } from "@/usecases/IUsecase/IPredictionUsecase"
-
-const todoRepository = new TodoRepository()
-export const todoUsecase: ITodoUsecase = new TodoUsecase(todoRepository)
 
 const topicRepository = new TopicRepository()
 export const topicUsecase: ITopicUsecase = new TopicUsecase(topicRepository)
